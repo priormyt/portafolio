716ed8c3b9891fa7832e119a7d2b5025b2568c79
priormyt <priormyt99@gmail.com>
2026-03-20 18:04:43 -0600

    chore: add github actions workflow for astro deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://priormyt.github.io',
+  base: '/portafolio',
   build: {
     format: 'file'
   }
