d647dc46f06ceb8457e8b67a1483c632153f0b1d
priormyt <priormyt99@gmail.com>
2026-03-20 18:17:11 -0600

    fix: use custom domain instead of base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://priormyt.github.io',
-  base: '/portafolio',
+  site: 'https://www.ante.photo',
   build: {
     format: 'file'
   }
